56311f6af03e52e8cdac9ee617b9e065ebfe5fed
Jon Burns <jonburns10@gmail.com>
2016-09-08 16:46:24 -0700

    layout.l creation

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.sliderA').slick({
+		dots: true,
+		slidesToShow: 1,
+		speed: 1000,
+		adaptiveHeight: true,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		arrows: false
+	})
+});
+
+$(document).foundation();
