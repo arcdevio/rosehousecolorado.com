3c77b9debfc6e8d8864ffc7f01bd5224e9a488d4
Jon Burns <jonburns10@gmail.com>
2016-09-08 17:41:40 -0700

    removed new version of jquery

---
+++ dist/program-overview/admissions/what-to-bring/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+	<title>The Rose House | Colorado Addiction Treatment Center</title>
+	<?php include_once($_SERVER['DOCUMENT_ROOT'].'/apps/head.php'); ?>
+</head>
+
+<body>
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/apps/menu-bar.php'); ?>
+
+
+
+
+
+
+
+<!-- Foot Section -->
+	<?php include_once($_SERVER['DOCUMENT_ROOT'].'/apps/foot.php'); ?>
---
+++ dist/program-overview/mission/brittany-rose/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+	<title>Brittany Rose | Mission | The Rose House</title>
+	<?php include_once($_SERVER['DOCUMENT_ROOT'].'/apps/head.php'); ?>
+</head>
+
+<body>
+<?php include_once($_SERVER['DOCUMENT_ROOT'].'/apps/menu-bar.php'); ?>
+
+<h1>Brittany Rose</h1>
+
+
+
+
+
+
+<!-- Foot Section -->
+	<?php include_once($_SERVER['DOCUMENT_ROOT'].'/apps/foot.php'); ?>
