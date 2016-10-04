3ca6ac6609e8c6e552e3b408add67787a5c6c0c0
PigeonJeorge <alex.steven.elias@gmail.com>
2016-10-04 13:36:24 -0700

    fixed astatine, adjusted insurance form

--- src/js/insurance-form.js
+++ src/js/insurance-form.js
@@ -1,7 +1,32 @@
 (function() {
 	'use strict';
 
-	var inusranceForm = document.querySelector('insurance-form');
+	var response = document.querySelector('.form-response');
+	var form = document.querySelector('.insurance-form');
 
+	At.setup.spinner.size = '6px';
+	At.setup.spinner.thickness = '30px';
+
+	At.submit({
+		mimeType: 'json',
+		query: '.insurance-form',
+		complete: function (error, success) {
+
+			if (error) {
+				var errorMessage = '';
+
+				try { errorMessage = JSON.parse(error.response).error; }
+				catch (e) { console.log(error); }
+
+				response.style.color = '#89293D';
+				response.innerText = 'Error ' + errorMessage;
+			} else {
+				form.style.display = 'none';
+				response.style.color = '#B0BF7F';
+				response.innerText = 'Verification Was Sent';
+			}
+
+		}
+	});
 
 }());
