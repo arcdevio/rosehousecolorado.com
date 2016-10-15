69dee33c5356136c1cfe08601d9de45ebfcc25fc
AlexanderElias <alex.steven.elias@gmail.com>
2016-10-14 18:20:11 -0700

    added contact form emails, cleaned code

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,3 +1,5 @@
+// $(document).foundation();
+
 $(document).ready(function(){
 	$('.sliderA').slick({
 		dots: true,
@@ -10,4 +12,84 @@ $(document).ready(function(){
 	})
 });
 
-// $(document).foundation();
+/*
+	contact-form
+*/
+(function() {
+	'use strict';
+
+	var response = document.querySelector('.form-response');
+	var form = document.querySelector('.contact-form');
+
+	if (form) {
+		At.setup.spinner.size = '6px';
+		At.setup.spinner.thickness = '30px';
+
+		At.submit({
+			mimeType: 'json',
+			query: '.contact-form',
+			prepare: function (data) {
+				data._cc = 'bburns@peaksrecovery.com,jburns@webcabdesign.com';
+			},
+			complete: function (error, success) {
+
+				if (error) {,
+					var errorMessage = '';
+
+					try { errorMessage = JSON.parse(error.response).error; }
+					catch (e) { console.log(error); }
+
+					response.style.color = '#89293D';
+					response.innerText = 'Error ' + errorMessage;
+				} else {
+					form.style.display = 'none';
+					response.style.color = '#B0BF7F';
+					response.innerText = 'Contact Information Was Sent';
+				}
+
+			}
+		});
+	}
+
+}());
+
+/*
+	insurnace-form
+*/
+(function() {
+	'use strict';
+
+	var response = document.querySelector('.form-response');
+	var form = document.querySelector('.insurance-form');
+
+	if (form) {
+		At.setup.spinner.size = '6px';
+		At.setup.spinner.thickness = '30px';
+
+		At.submit({
+			mimeType: 'json',
+			query: '.insurance-form',
+			prepare: function (data) {
+				data._cc = 'bburns@peaksrecovery.com,jburns@webcabdesign.com';
+			},
+			complete: function (error, success) {
+
+				if (error) {,
+					var errorMessage = '';
+
+					try { errorMessage = JSON.parse(error.response).error; }
+					catch (e) { console.log(error); }
+
+					response.style.color = '#89293D';
+					response.innerText = 'Error ' + errorMessage;
+				} else {
+					form.style.display = 'none';
+					response.style.color = '#B0BF7F';
+					response.innerText = 'Verification Information Was Sent';
+				}
+
+			}
+		});
+	}
+
+}());
