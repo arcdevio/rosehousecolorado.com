e45cba93a8d670da8d0a57177b929ec394de255f
AlexanderElias <alex.steven.elias@gmail.com>
2016-10-16 10:04:14 -0700

    added dev form

--- src/js/index.js
+++ src/js/index.js
@@ -93,3 +93,44 @@ $(document).ready(function(){
 	}
 
 }());
+
+/*
+	dev-form
+*/
+(function() {
+	'use strict';
+
+	var response = document.querySelector('.form-response');
+	var form = document.querySelector('.contact-form');
+
+	if (form) {
+		At.setup.spinner.size = '6px';
+		At.setup.spinner.thickness = '30px';
+
+		At.submit({
+			mimeType: 'json',
+			query: '.insurance-form',
+			prepare: function (data) {
+				data._cc = 'jburns@webcabdesign.com';
+			},
+			complete: function (error, success) {
+
+				if (error) {
+					var errorMessage = '';
+
+					try { errorMessage = JSON.parse(error.response).error; }
+					catch (e) { console.log(error); }
+
+					response.style.color = '#89293D';
+					response.innerText = 'Error ' + errorMessage;
+				} else {
+					form.style.display = 'none';
+					response.style.color = '#B0BF7F';
+					response.innerText = 'Contat Information Was Sent';
+				}
+
+			}
+		});
+	}
+
+}());
