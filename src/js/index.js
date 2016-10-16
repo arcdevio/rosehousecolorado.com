6b58ec5f80974ffe924c7730187e8f1fd2448b36
AlexanderElias <alex.steven.elias@gmail.com>
2016-10-16 10:48:57 -0700

    upload test

--- src/js/index.js
+++ src/js/index.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 			mimeType: 'json',
 			query: '.contact-form',
 			prepare: function (data) {
-				data._cc = 'bburns@peaksrecovery.com';
+				// data._cc = 'bburns@peaksrecovery.com';
 			},
 			complete: function (error, success) {
 
@@ -97,40 +97,40 @@ $(document).ready(function(){
 /*
 	dev-form
 */
-(function() {
-	'use strict';
-
-	var response = document.querySelector('.form-response');
-	var form = document.querySelector('.contact-form');
-
-	if (form) {
-		At.setup.spinner.size = '6px';
-		At.setup.spinner.thickness = '30px';
-
-		At.submit({
-			mimeType: 'json',
-			query: '.insurance-form',
-			prepare: function (data) {
-				data._cc = 'jburns@webcabdesign.com';
-			},
-			complete: function (error, success) {
-
-				if (error) {
-					var errorMessage = '';
-
-					try { errorMessage = JSON.parse(error.response).error; }
-					catch (e) { console.log(error); }
-
-					response.style.color = '#89293D';
-					response.innerText = 'Error ' + errorMessage;
-				} else {
-					form.style.display = 'none';
-					response.style.color = '#B0BF7F';
-					response.innerText = 'Contat Information Was Sent';
-				}
-
-			}
-		});
-	}
-
-}());
+// (function() {
+// 	'use strict';
+//
+// 	var response = document.querySelector('.form-response');
+// 	var form = document.querySelector('.contact-form');
+//
+// 	if (form) {
+// 		At.setup.spinner.size = '6px';
+// 		At.setup.spinner.thickness = '30px';
+//
+// 		At.submit({
+// 			mimeType: 'json',
+// 			query: '.contact-form',
+// 			prepare: function (data) {
+// 				data._cc = 'jburns@webcabdesign.com';
+// 			},
+// 			complete: function (error, success) {
+//
+// 				if (error) {
+// 					var errorMessage = '';
+//
+// 					try { errorMessage = JSON.parse(error.response).error; }
+// 					catch (e) { console.log(error); }
+//
+// 					response.style.color = '#89293D';
+// 					response.innerText = 'Error ' + errorMessage;
+// 				} else {
+// 					form.style.display = 'none';
+// 					response.style.color = '#B0BF7F';
+// 					response.innerText = 'Contat Information Was Sent';
+// 				}
+//
+// 			}
+// 		});
+// 	}
+//
+// }());
