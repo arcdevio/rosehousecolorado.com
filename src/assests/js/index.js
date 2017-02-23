4f061f01ca6c85537cdc9268e1f9def27460f6b3
Jon Burns <jonburns10@gmail.com>
2017-02-22 21:04:55 -0700

    CC'd marcie

--- src/assests/js/index.js
+++ src/assests/js/index.js
@@ -71,7 +71,7 @@ $(document).ready(function(){
 			action: 'https://www.enformed.io/zpn17s0',
 			prepare: function (data, resolve) {
 				data['*default_email'] = 'amatino@rosehousecolorado.com';
-				data['*cc'] = 'lindseya@rosehousecolorado.com,krosen@rosehousecolorado.com';
+				data['*cc'] = 'lindseya@rosehousecolorado.com,krosen@rosehousecolorado.com,mchambers101@gmail.com';
 				resolve(data);
 			},
 			complete: function (error, success) {
