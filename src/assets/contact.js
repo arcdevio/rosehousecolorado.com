090c50b66f6db807cdb8da66b3392f91b0e1809f
Alexander Elias <alex.steven.elias@gmail.com>
2018-12-11 10:49:25 -0700

    removed comments

--- src/assets/contact.js
+++ src/assets/contact.js
@@ -14,9 +14,9 @@
 		responseType: 'json',
 		action: 'https://www.enformed.io/zpn17s0',
 		prepare: function (data, resolve) {
-			// data['*default_email'] = 'ahill@rosehousecolorado.com';
-			// data['*cc'] = 'mchambers101@gmail.com,cconger@rosehousecolorado.com';
-			// resolve(data);
+			data['*default_email'] = 'ahill@rosehousecolorado.com';
+			data['*cc'] = 'mchambers101@gmail.com,cconger@rosehousecolorado.com';
+			resolve(data);
 		},
 		complete: function (error, success) {
 			if (error) {
