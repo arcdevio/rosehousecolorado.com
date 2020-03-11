109bab7f9234d6e1405eecf9410f6afda14ce167
Jon Burns <jonburns10@gmail.com>
2020-03-10 20:46:24 -0700

    build

--- dev/program-overview/admissions/verify-insurance/index.js
+++ dev/program-overview/admissions/verify-insurance/index.js
@@ -15,7 +15,7 @@
 		action: 'https://www.enformed.io/csl0xqaw',
 		prepare: function (data, resolve) {
 			data['*default_email'] = 'ahill@rosehousecolorado.com';
-			data['*cc'] = 'mchambers101@gmail.com,cconger@rosehousecolorado.com';
+			data['*cc'] = 'mchambers101@gmail.com,ngrebe@rosehousecolorado.com';
 			resolve(data);
 		},
 		complete: function (error, success) {
