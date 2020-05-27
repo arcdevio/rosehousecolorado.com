352055dba17bec1683644979081730199ac6573a
Alexander Elias <alex.steven.elias@gmail.com>
2020-05-27 11:12:16 -0700

    updated verification form

--- src/program-overview/admissions/verify-insurance/index.js
+++ src/program-overview/admissions/verify-insurance/index.js
@@ -5,29 +5,43 @@
 	var form = document.querySelector('.insurance-form');
 	if (!form) return;
 
-	At.setup.spinner.size = '6px';
-	At.setup.spinner.thickness = '30px';
+    form.addEventListener('submit', function (e) {
+		response.innerText = 'Submitting Form';
 
-	At.submit({
-		query: form,
-		method: 'post',
-		responseType: 'json',
-		action: 'https://www.enformed.io/csl0xqaw',
-		prepare: function (data, resolve) {
-			data['*default_email'] = 'ahill@rosehousecolorado.com';
-			data['*cc'] = 'mchambers101@gmail.com,ngrebe@rosehousecolorado.com';
-			resolve(data);
-		},
-		complete: function (error, success) {
-			if (error) {
-				response.style.color = '#89293D';
-				response.innerText = 'Error Please Call';
-			} else {
-				form.style.display = 'none';
-				response.style.color = '#B0BF7F';
-				response.innerText = 'Verification Form Submitted';
-			}
-		}
-	});
+        e.preventDefault();
+
+        var data = {};
+        var inputs = form.querySelectorAll('input');
+        for (var i = 0; i < inputs.length; i++) {
+            var input = inputs[i];
+            var name = input.name;
+            var value = input.value;
+            if (name) data[name] = value;
+        }
+
+    	// data['*cc'] = '';
+    	// data['*default_email'] = 'alex.steven.elias@gmail.com';
+		data['*default_email'] = 'ahill@rosehousecolorado.com';
+		data['*cc'] = 'mchambers101@gmail.com,ngrebe@rosehousecolorado.com';
+
+        fetch('https://www.enformed.io/csl0xqaw/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(function (response) {
+            response.json();
+        }).then(function (data) {
+    		form.style.display = 'none';
+    		response.style.color = '#B0BF7F';
+    		response.innerText = 'Submitted Form';
+        }).catch(function (error) {
+    		response.style.color = '#89293D';
+    		response.innerText = 'Error Please Call';
+        });
+
+    });
 
 }());
