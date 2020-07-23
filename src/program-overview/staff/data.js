65292694c8ecbf5c01221ef37ea2a17bcb2c8945
Jon Burns <jonburns10@gmail.com>
2020-07-23 13:48:33 -0700

    staff page update

--- src/program-overview/staff/data.js
+++ src/program-overview/staff/data.js
@@ -18,12 +18,12 @@ window.Global.staff = [
 		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,g_face,h_900,q_60,w_600/v1475687598/rose-house/profile-picture/Dr.jpg',
 		descriptions: ['Dr. William received his medical degree from University of Benin College of Medicine. He was Chief Resident at the University at Harlem Hospital Center, College of Physicians &amp; Surgeons of Columbia. Dr. William is certified by the American Board of Psychiatry and Neurology. He is a Fellow of the American Psychiatric Association, board certified by the American Board of Addiction Medicine (ABAM) and Member of the Colorado Psychiatric Society.']
 	},{
-		name: 'Alex Griffin, RN',
+		name: 'Paula Snow, RN',
 		title: 'Director of Nursing',
-		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,g_face,h_900,w_600/v1542240724/rose-house/profile-picture/Alex_Griffin_photo.jpg',
-		descriptions: ['Alex earned a Bachelors of Science in Nursing from University of Colorado Anschutz Medical Campus as well as a Bachelor of Science in Neuroscience and Psychology from the University of Colorado Boulder. She has experience working as a RN with people rehabilitating from traumatic brain and spinal cord injuries, patients undergoing withdraw from substance use and those recovering from eating disorders. In addition she has worked as a recreational therapeutic instructor alongside individuals with developmental delays. She has a passion for rehabilitation therapy. Her focus is the therapeutic process and being able to engage with patients continually as they develop along their path. Alex is honored to work with Rose House and to be a part of the recovery process.']
+		image: '/program-overview/staff/images/Paula_Snow.jpg',
+		descriptions: ['Paula has been in the nursing field for over 25 years, with the bulk of her career in Women’s Health. Empowering women to grow and achieve their personal best is her primary focus. She strives to learn from every person she meets while living life to its fullest.']
 	},{
-		name: 'Barbara Flood, PhD',
+		name: 'Barbara Flood, LAC, PhD',
 		title: 'Primary Therapist',
 		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_scale,f_auto,h_900,w_600/v1533316642/rose-house/Barbara_Flood_pic.jpg',
 		descriptions: ['Dr. Barbara Flood holds a PhD in Transpersonal Psychology and an MSW from the University of Wisconsin Milwaukee. Dr. Flood is a trained addictions counselor and a trauma therapist who has been treating clients for over 30 years. She has extensive training in addressing traumatic events such as childhood sexual, emotional, or physical abuse; rape or sexual assault; intimate partner violence; traumatic grief; and general traumatic events. Dr. Flood has extensive training in grief and loss, Gestalt Therapy, trauma reduction, mindfulness, Play, Sand and Art therapy and Dialectical Behavior Therapy.','Dr. Flood has taught psychology in several university, graduate and undergraduate programs including Naropa, CU Denver, and Metropolitan University of Denver.']
@@ -40,7 +40,7 @@ window.Global.staff = [
 	// 	descriptions: ['']
 	// },
 	{
-		name: 'Rebecca Glasmann',
+		name: 'Rebecca Glasmann, CACII',
 		title: 'Step Down Manager',
 		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,h_900,w_600/v1573762385/rose-house/20191113_144237.jpg',
 		descriptions: ['Rebecca is originally from South Dakota. She has been in Colorado now for over 20 years and grateful to live in such a beautiful place. She enjoys spending time in the mountains especially in the Fall. She feels blessed to be the mother of three beautiful children. She is currently working on her Associates in Psychology and her CAC I. In recovery herself, she knows the journey these women are embarking on and is passionate about sharing her experience, strength and hope each step of the way. In addition to supporting the women in the house, she works as the Family Coordinator to give support and resources to family members so they may better support their loved ones and themselves. Addiction and mental illness is a family disease and it is important that each member of the family get the care and support they need. She is grateful to be a part of an amazing team at the Rose House and have the chance to help other women discover what a gift they are.']
@@ -50,7 +50,7 @@ window.Global.staff = [
 		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_scale,f_auto,w_600/v1572315259/rose-house/KathyPahler.jpg',
 		descriptions: ['Originally from North Carolina, Kathy is a graduate of The University of Colorado, Boulder and has called Colorado home for three decades. She has many years of experience in sales and marketing which she brings to her position as The Director of Business Development. Being a part of The Rose House community appeals to her commitment to living a healthy and conscious lifestyle. In her free time, Kathy practices yoga, enjoys reading, biking, and spending time with her husband and two adult children.']
 	},{
-		name: 'Sandra Radney',
+		name: 'Sandra Radney, CACII',
 		title: 'Family Coordinator',
 		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,g_face,h_900,w_600/v1542240179/rose-house/profile-picture/Sandra_Radney_photo.jpg',
 		descriptions: ['Sandra grew up in Wyoming. She earned a BS in Education from Utah State University and her MA in Education from Chapman University. Sandra taught middle and high school students with behavioral, emotional and learning disabilities for years and was the Community Outreach/Prevention Coordinator for the local high school in Wisconsin where she lived with her family for 15 years. Sandra has training in drug and alcohol awareness and prevention, including a CAC I, SAP, Brief Intervention and Motivational Interviewing. She is currently working on a CAC II. Sandra is grateful for the opportunity to support beautiful, brave women in recovery and their families, as well as be a part of the amazing Rose House staff.','Sandra enjoys living in the beautiful, sunny state of Colorado with her husband, daughter and two sons and all of the outdoor activities it offers including running, hiking, camping and skiing. She is an avid reader, knitter and traveler.']
