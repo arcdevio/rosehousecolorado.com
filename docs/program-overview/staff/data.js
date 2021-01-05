e6f90225f460feb6f182f4fdb0c468f850c878c8
Jon Burns <jonburns10@gmail.com>
2021-01-05 13:54:41 -0800

    phone number update, build

--- docs/program-overview/staff/data.js
+++ docs/program-overview/staff/data.js
@@ -1,58 +1,63 @@
+
 window.Global = window.Global || {};
-window.Global.staff = [{
-  name: 'Marcie Chambers, PhD',
-  title: 'CEO &amp; Founder',
-  image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,g_faces,h_900,q_60,w_600/v1475687642/rose-house/profile-picture/Marcie_Chambers_Ph.D.jpg',
-  descriptions: ['Dr. Marcie Chambers received her PhD in Psychology from the University of Colorado at Boulder. She was in private practice for a number of years, treating clients for depression, anxiety, trauma, codependency and addiction. Her practice included treating individuals, couples, and families. Dr. Chambers taught psychology at the University of Colorado, Regis University and Metro State College. Dr. Chambers opened The Rose House in Boulder County, Colorado in 2008.']
-}, {
-  name: 'Allison Hill',
-  title: 'Executive Director',
-  image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,g_faces,h_900,q_60,w_600/v1475687551/rose-house/profile-picture/Allison_Hein_House_Manager.png',
-  descriptions: ['Allison lives in Morrison, Colorado, close to the beautiful Red Rocks. She loves going outside, being a mom and spending quality time with her family. Allison is a Certified Addiction Counselor Level II, registered with the State of Colorado.. She is passionate about Recovery, in Love with Life and feels very fortunate to be a part of the Rose House team. Having completed the Rose House program herself, Allison is grateful for the opportunity to support women on their journey of recovery!']
-}, {
-  name: 'Arshad William, MD',
-  title: 'Medical Director',
-  image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,g_face,h_900,q_60,w_600/v1475687598/rose-house/profile-picture/Dr.jpg',
-  descriptions: ['Dr. William received his medical degree from University of Benin College of Medicine. He was Chief Resident at the University at Harlem Hospital Center, College of Physicians &amp; Surgeons of Columbia. Dr. William is certified by the American Board of Psychiatry and Neurology. He is a Fellow of the American Psychiatric Association, board certified by the American Board of Addiction Medicine (ABAM) and Member of the Colorado Psychiatric Society.']
-}, {
-  name: 'Elisabeth Fabrizio, RN',
-  title: 'Director of Nursing',
-  image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,h_900,w_600/v1608148758/rose-house/profile-picture/20201208_140537.jpg',
-  descriptions: ['As a registered nurse and also a family nurse practitioner, I have called Colorado home for over 15 years. Having worked in addiction medicine for almost five years, I am very happy to be working at The Rose House with women in recovery. In addition to being the nurse at The Rose House, I also have a position in a detox facility. In my free time, I enjoy spending time with my daughters, my beagles, or baking some new recipe. I am always willing to discuss health concerns, medical questions, and anything related to health and wellness.']
-}, {
-  name: 'Barbara Flood, LAC, PhD',
-  title: 'Primary Therapist',
-  image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_scale,f_auto,h_900,w_600/v1533316642/rose-house/Barbara_Flood_pic.jpg',
-  descriptions: ['Dr. Barbara Flood holds a PhD in Transpersonal Psychology and an MSW from the University of Wisconsin Milwaukee. Dr. Flood is a trained addictions counselor and a trauma therapist who has been treating clients for over 30 years. She has extensive training in addressing traumatic events such as childhood sexual, emotional, or physical abuse; rape or sexual assault; intimate partner violence; traumatic grief; and general traumatic events. Dr. Flood has extensive training in grief and loss, Gestalt Therapy, trauma reduction, mindfulness, Play, Sand and Art therapy and Dialectical Behavior Therapy.', 'Dr. Flood has taught psychology in several university, graduate and undergraduate programs including Naropa, CU Denver, and Metropolitan University of Denver.']
-}, {
-  name: 'Larry Algiene LPC, CACII',
-  title: 'Primary Therapist',
-  image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,h_900,w_600/v1573594460/rose-house/IMG_0104.jpg',
-  descriptions: ['Larry Algiene is a Licensed Professional Counselor in the state of Colorado and has been practicing since 2008. He has a BS in Computer Information Systems from Colorado Christian University, MA in Clinical and School Counseling from the University of Northern Colorado and is a Certified Addictions Counselor. Larry has extensive experience treating trauma and excels in supporting clients with emotional development challenges.  Larry is trained in CBT, Post Induction Therapy, EMDR and Family Systems management.  He has worked with emotional development in Parole, Probation and Residential settings for over twenty years.  Prior to coming to the Rose House, Larry worked as the Drug Court Mental Health Therapist in Weld County and was a member of the Colorado Army National Guard for 17 years.  Larry has been married for 17 years and spends all of his free time chasing his son and daughter.']
-}, // {
-// 	name: 'Tanisha Cordova',
-// 	title: 'House Manager',
-// 	image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,g_face,h_900,w_600/v1475687687/rose-house/profile-picture/Tanisha_Cordova_Psychiatric_Technician.jpg',
-// 	descriptions: ['']
-// },
-{
-  name: 'Rebecca Glasmann, CACII',
-  title: 'Step Down Manager',
-  image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,h_900,w_600/v1573762385/rose-house/20191113_144237.jpg',
-  descriptions: ['Rebecca is originally from South Dakota. She has been in Colorado now for over 20 years and grateful to live in such a beautiful place. She enjoys spending time in the mountains especially in the Fall. She feels blessed to be the mother of three beautiful children. She is currently working on her Associates in Psychology and her CAC I. In recovery herself, she knows the journey these women are embarking on and is passionate about sharing her experience, strength and hope each step of the way. In addition to supporting the women in the house, she works as the Family Coordinator to give support and resources to family members so they may better support their loved ones and themselves. Addiction and mental illness is a family disease and it is important that each member of the family get the care and support they need. She is grateful to be a part of an amazing team at the Rose House and have the chance to help other women discover what a gift they are.']
-}, {
-  name: 'Kathy Pahler',
-  title: 'Director of Business Development',
-  image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_scale,f_auto,w_600/v1572315259/rose-house/KathyPahler.jpg',
-  descriptions: ['Originally from North Carolina, Kathy is a graduate of The University of Colorado, Boulder and has called Colorado home for three decades. She has many years of experience in sales and marketing which she brings to her position as The Director of Business Development. Being a part of The Rose House community appeals to her commitment to living a healthy and conscious lifestyle. In her free time, Kathy practices yoga, enjoys reading, biking, and spending time with her husband and two adult children.']
-}, {
-  name: 'Sandra Radney, CACII',
-  title: 'Family Coordinator',
-  image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,g_face,h_900,w_600/v1542240179/rose-house/profile-picture/Sandra_Radney_photo.jpg',
-  descriptions: ['Sandra grew up in Wyoming. She earned a BS in Education from Utah State University and her MA in Education from Chapman University. Sandra taught middle and high school students with behavioral, emotional and learning disabilities for years and was the Community Outreach/Prevention Coordinator for the local high school in Wisconsin where she lived with her family for 15 years. Sandra has training in drug and alcohol awareness and prevention, including a CAC I, SAP, Brief Intervention and Motivational Interviewing. She is currently working on a CAC II. Sandra is grateful for the opportunity to support beautiful, brave women in recovery and their families, as well as be a part of the amazing Rose House staff.', 'Sandra enjoys living in the beautiful, sunny state of Colorado with her husband, daughter and two sons and all of the outdoor activities it offers including running, hiking, camping and skiing. She is an avid reader, knitter and traveler.']
-}, {
-  name: 'Cristy Harland',
-  title: 'Administrator',
-  image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,g_face,h_900,w_600/v1533317668/rose-house/IMG_3046.jpg',
-  descriptions: ['Cristy grew up in Boulder and loved it so much that she stayed and attended CU. Cristy graduated from the University of Colorado’s Leeds School of Business.', 'Cristy’s interests include volunteering, hiking, reading and traveling. She is a big sports fan and enjoys watching ice hockey and football. Her most treasured times are with her husband and two sons.']
-}];
+
+window.Global.staff = [
+	{
+		name: 'Marcie Chambers, PhD',
+		title: 'CEO &amp; Founder',
+		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,g_faces,h_900,q_60,w_600/v1475687642/rose-house/profile-picture/Marcie_Chambers_Ph.D.jpg',
+		descriptions: ['Dr. Marcie Chambers received her PhD in Psychology from the University of Colorado at Boulder. She was in private practice for a number of years, treating clients for depression, anxiety, trauma, codependency and addiction. Her practice included treating individuals, couples, and families. Dr. Chambers taught psychology at the University of Colorado, Regis University and Metro State College. Dr. Chambers opened The Rose House in Boulder County, Colorado in 2008.']
+	},{
+		name: 'Allison Hill',
+		title: 'Executive Director',
+		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,g_faces,h_900,q_60,w_600/v1475687551/rose-house/profile-picture/Allison_Hein_House_Manager.png',
+		descriptions: ['Allison lives in Morrison, Colorado, close to the beautiful Red Rocks. She loves going outside, being a mom and spending quality time with her family. Allison is a Certified Addiction Counselor Level II, registered with the State of Colorado.. She is passionate about Recovery, in Love with Life and feels very fortunate to be a part of the Rose House team. Having completed the Rose House program herself, Allison is grateful for the opportunity to support women on their journey of recovery!']
+	},{
+		name: 'Arshad William, MD',
+		title: 'Medical Director',
+		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,g_face,h_900,q_60,w_600/v1475687598/rose-house/profile-picture/Dr.jpg',
+		descriptions: ['Dr. William received his medical degree from University of Benin College of Medicine. He was Chief Resident at the University at Harlem Hospital Center, College of Physicians &amp; Surgeons of Columbia. Dr. William is certified by the American Board of Psychiatry and Neurology. He is a Fellow of the American Psychiatric Association, board certified by the American Board of Addiction Medicine (ABAM) and Member of the Colorado Psychiatric Society.']
+	},{
+		name: 'Elisabeth Fabrizio, RN, FNP',
+		title: 'Director of Nursing',
+		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,h_900,w_600/v1608148758/rose-house/profile-picture/20201208_140537.jpg',
+		descriptions: ['As a registered nurse and also a family nurse practitioner, I have called Colorado home for over 15 years. Having worked in addiction medicine for almost five years, I am very happy to be working at The Rose House with women in recovery. In addition to being the nurse at The Rose House, I also have a position in a detox facility. In my free time, I enjoy spending time with my daughters, my beagles, or baking some new recipe. I am always willing to discuss health concerns, medical questions, and anything related to health and wellness.']
+	},{
+		name: 'Barbara Flood, LAC, PhD',
+		title: 'Primary Therapist',
+		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_scale,f_auto,h_900,w_600/v1533316642/rose-house/Barbara_Flood_pic.jpg',
+		descriptions: ['Dr. Barbara Flood holds a PhD in Transpersonal Psychology and an MSW from the University of Wisconsin Milwaukee. Dr. Flood is a trained addictions counselor and a trauma therapist who has been treating clients for over 30 years. She has extensive training in addressing traumatic events such as childhood sexual, emotional, or physical abuse; rape or sexual assault; intimate partner violence; traumatic grief; and general traumatic events. Dr. Flood has extensive training in grief and loss, Gestalt Therapy, trauma reduction, mindfulness, Play, Sand and Art therapy and Dialectical Behavior Therapy.','Dr. Flood has taught psychology in several university, graduate and undergraduate programs including Naropa, CU Denver, and Metropolitan University of Denver.']
+	},{
+		name: 'Larry Algiene LPC, CACII',
+		title: 'Primary Therapist',
+		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,h_900,w_600/v1573594460/rose-house/IMG_0104.jpg',
+		descriptions: ['Larry Algiene is a Licensed Professional Counselor in the state of Colorado and has been practicing since 2008. He has a BS in Computer Information Systems from Colorado Christian University, MA in Clinical and School Counseling from the University of Northern Colorado and is a Certified Addictions Counselor. Larry has extensive experience treating trauma and excels in supporting clients with emotional development challenges.  Larry is trained in CBT, Post Induction Therapy, EMDR and Family Systems management.  He has worked with emotional development in Parole, Probation and Residential settings for over twenty years.  Prior to coming to the Rose House, Larry worked as the Drug Court Mental Health Therapist in Weld County and was a member of the Colorado Army National Guard for 17 years.  Larry has been married for 17 years and spends all of his free time chasing his son and daughter.']
+	},
+	// {
+	// 	name: 'Tanisha Cordova',
+	// 	title: 'House Manager',
+	// 	image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,g_face,h_900,w_600/v1475687687/rose-house/profile-picture/Tanisha_Cordova_Psychiatric_Technician.jpg',
+	// 	descriptions: ['']
+	// },
+	{
+		name: 'Rebecca Glasmann, CACII',
+		title: 'Step Down Manager',
+		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,h_900,w_600/v1573762385/rose-house/20191113_144237.jpg',
+		descriptions: ['Rebecca is originally from South Dakota. She has been in Colorado now for over 20 years and grateful to live in such a beautiful place. She enjoys spending time in the mountains especially in the Fall. She feels blessed to be the mother of three beautiful children. She is currently working on her Associates in Psychology and her CAC I. In recovery herself, she knows the journey these women are embarking on and is passionate about sharing her experience, strength and hope each step of the way. In addition to supporting the women in the house, she works as the Family Coordinator to give support and resources to family members so they may better support their loved ones and themselves. Addiction and mental illness is a family disease and it is important that each member of the family get the care and support they need. She is grateful to be a part of an amazing team at the Rose House and have the chance to help other women discover what a gift they are.']
+	},{
+		name: 'Kathy Pahler',
+		title: 'Director of Business Development',
+		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_scale,f_auto,w_600/v1572315259/rose-house/KathyPahler.jpg',
+		descriptions: ['Originally from North Carolina, Kathy is a graduate of The University of Colorado, Boulder and has called Colorado home for three decades. She has many years of experience in sales and marketing which she brings to her position as The Director of Business Development. Being a part of The Rose House community appeals to her commitment to living a healthy and conscious lifestyle. In her free time, Kathy practices yoga, enjoys reading, biking, and spending time with her husband and two adult children.']
+	},{
+		name: 'Sandra Radney, CACII',
+		title: 'Family Coordinator',
+		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,g_face,h_900,w_600/v1542240179/rose-house/profile-picture/Sandra_Radney_photo.jpg',
+		descriptions: ['Sandra grew up in Wyoming. She earned a BS in Education from Utah State University and her MA in Education from Chapman University. Sandra taught middle and high school students with behavioral, emotional and learning disabilities for years and was the Community Outreach/Prevention Coordinator for the local high school in Wisconsin where she lived with her family for 15 years. Sandra has training in drug and alcohol awareness and prevention, including a CAC I, SAP, Brief Intervention and Motivational Interviewing. She is currently working on a CAC II. Sandra is grateful for the opportunity to support beautiful, brave women in recovery and their families, as well as be a part of the amazing Rose House staff.','Sandra enjoys living in the beautiful, sunny state of Colorado with her husband, daughter and two sons and all of the outdoor activities it offers including running, hiking, camping and skiing. She is an avid reader, knitter and traveler.']
+	},{
+		name: 'Cristy Harland',
+		title: 'Administrator',
+		image: 'https://res.cloudinary.com/dbc2wlvk8/image/upload/c_fill,f_auto,g_face,h_900,w_600/v1533317668/rose-house/IMG_3046.jpg',
+		descriptions: ['Cristy grew up in Boulder and loved it so much that she stayed and attended CU. Cristy graduated from the University of Colorado’s Leeds School of Business.','Cristy’s interests include volunteering, hiking, reading and traveling. She is a big sports fan and enjoys watching ice hockey and football. Her most treasured times are with her husband and two sons.']
+	}
+];
